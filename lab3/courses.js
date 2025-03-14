8584984b8451d5242140a160819fa53a3d7c8ade
Vu Truong Giang <vutruonggiangty@gmail.com>
2025-03-14 14:51:12 +0700

    courses.js

--- lab3/courses.js
+++ lab3/courses.js
@@ -3,7 +3,17 @@ document.getElementById("myForm").addEventListener("submit" , function(even){
 
     let name = document.getElementById("name").value
     let password = document.getElementById("password").value
+    let passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/;
+    if(!passwordPattern.test(password)){
+        alert("Mật khẩu phải có ít nhất 8 ký tự, gồm ít nhất 1 chữ hoa, 1 chữ thường, 1 số và 1 ký tự đặc biệt.");
+        return;
+    }
     let email = document.getElementById("email").value
+    let emailPattern = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]{2,}$/i
+    if(!emailPattern.test(email)){
+        alert("email có dạng abc@gmail.com")
+        return;
+    }
 
     let gender = document.querySelector('input[name="gender"]:checked')
     gender = gender ? gender.value : "Not value"
